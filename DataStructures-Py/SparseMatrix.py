b15edccbc5bf7c7c8e7b299424f191f0ea4b151f
StrStark <rezafathisamani1383@gmail.com>
2024-11-11 15:31:53 +0330

    sparse changed

--- DataStructures-Py/SparseMatrix.py
+++ DataStructures-Py/SparseMatrix.py
@@ -1,32 +1,51 @@
 class SparseMatrix:
-    __rows =0
-    __cols = 0
-    __Index=0
-    __Data =[]
-    def __init__(self, rows, cols):
-        self.__rows = rows  
-        self.__cols = cols  
-        self.__Data = [None]*rows  
+    __Rows = 0
+    __Cols = 0
+    __Index = 0
+    __Data = []
+
+    def __init__(self, rows , cols) :
+        self.__Rows = rows
+        self.__Cols = cols
+        self.__Data = [None] * rows
+
     def GetRow(self):
-        return self.__rows
+        return self.__Rows
+    
     def GetColumn(self):
         return self.__cols
-    def Add(self, i, j, value):
-        if i >= self.__rows or j >= self.__cols:
-            raise IndexError("Index out of bounds!")
-        
-        if value != 0:  
-            self.__Data[self.__Index]=[i, j, value]
+    
 
-    def Get(self, i, j):
-        if i >= self.__rows or j >= self.__cols:
-            raise IndexError("Index out of bounds!")
+    def Add(self, i , j, value) :
+        if self.__Index == self.__Rows :
+            raise OverflowError("Out of memory")
         
+        if i <0 or j <0 or i >=self.__Rows or j >= self.__Cols :
+            raise IndexError("Index out of bounds!")
+        if(value == 0):
+            raise ValueError("Value can not be zero")
+        
+        for i in range(0 ,self.__Index):
+            if self.__Data[i][0] == i and self.__Data[i][1] == j:
+                if self.__Data[i][2] != value :
+                    raise ValueError("This value Exist in the matrix , try changing it")
+                else :
+                    return
+                
+        self.__Data[self.__Index] = [i, j , value]
+        self.__Index +=1
+
+    def Get(self , i , j):
+        if i <0 or j <0 or i >=self.__Rows or j >= self.__Cols :
+            raise IndexError("Index out of bounds!")
         for entry in self.__Data:
-            if entry[0] == i and entry[1] == j:
-                return entry[2]
-        return 0  
-
+            if entry == None :
+                break
+            if entry[0] == i and entry[1] == j :
+                return entry
+        
+        raise ValueError(f"No item is saved at {i} - {j}")
+        
     def print_matrix(self):
         x = "Row Index | Column Index | Value"
         print(x ,"\n", "-"*len(x))
