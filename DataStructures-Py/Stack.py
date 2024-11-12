3111aa0c624f1029799b3fa90f99e9a2f186f832
StrStark <rezafathisamani1383@gmail.com>
2024-11-12 18:13:08 +0330

    Stack Updated

--- DataStructures-Py/Stack.py
+++ DataStructures-Py/Stack.py
@@ -1,55 +1,59 @@
-class Stack :
+class Stack:
+    __Data =[]
+    __Max_Capacity = 0
+    __Top = -1
     
-    __Data = []
-    __Max_Copacity = 0
-    __Top = 0
-    
-    def __init__(self, Copacity):
-        self.__Data = [None] * Copacity
-        self.__Max_Capacity = Copacity
-        self.__Top = -1
+    def __init__(self , capacity):
+        self.__Data = [None] * capacity
+        self.__Max_Capacity = capacity
         
     def __str__(self):
         return f"{self.__Data}"
     
-    @property
-    def Max_Capacity(self):
-        return self.__Max_Capacity
-    
     def IsFull(self) :
-        return self.__Top == self.Max_Capacity-1
+        return self.__Top == self.__Max_Capacity-1
     
     def IsEmpty(self) :
         return self.__Top == -1
     
-    def Push(self , value):
+    def Push(self , value) :
         if self.IsFull() :
-            raise OverflowError("Stack OverFlow")
+            raise OverflowError("Stack OverFlow")    
         
         self.__Top +=1
         self.__Data[self.__Top] = value
-    
+        
     def Pop(self):
         if self.IsEmpty() :
-            raise OverflowError("Stack UnderFlow")
+            raise OverflowError("Stack UnderFlow , You are trying to pop from an empty list")
         
         self.__Top -=1
-        return self.__Data[self.__Top +1]
+        return self.__Data[self.__Top+1]
     
-    def Peak(self) :
+    def Peak(self):
         if self.IsEmpty() :
-            raise OverflowError("Stack UnderFlow")
+            raise OverflowError("Stack UnderFlow , You are trying to peak from an empty list")
         
         return self.__Data[self.__Top]
 
     def IncreaseCopacity(self,NewCop):
-        self.__Max_Copacity = NewCop
-        remaning = NewCop - self.Max_Capacity
+        remaning = NewCop - self.__Max_Capacity
         
         while remaning :
             self.__Data.append(None)
             remaning -=1
-    
+        
+        self.__Max_Capacity = NewCop
+            
+    def DecreaseCopacity(self , NewCap):
+        if NewCap < self.__Top:
+            raise ValueError("Can not deacrese the capacity less than the count of values inside the stack")
+        remaning = self.__Max_Capacity - NewCap
+        while remaning :
+            self.__Data.pop()
+            remaning -=1
+        self.__Max_Capacity = NewCap
+
 x = Stack(5)
 try :
     x.Push("Hello")
