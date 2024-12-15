716216d1e981cf8a410fb7cf8a97608264874c45
StrStark <rezafathisamani1383@gmail.com>
2024-12-16 01:41:30 +0330

    fixed bug

--- DataStructures-Py/LinkedList/LinkedList.py
+++ DataStructures-Py/LinkedList/LinkedList.py
@@ -18,7 +18,18 @@ class LinkedList :
                 result += " -> "
             temp_node = temp_node.next_node
         return result
-    
+    def GetNodeWithValue(self , value):
+        if not isinstance(value , self.datatype):
+            raise TypeError(f"linkedList only accepts data of type :{self.datatype}!")
+        
+        current = self.Head
+        
+        while current.next_node is not None :
+            if current.Value == value:
+                return current
+            current = current.next_node
+            
+        raise ValueError(f'Value : {value} did not found in linekdList!')
     def Insert(self , value):
         if not isinstance(value , self.datatype):
             raise TypeError(f"linkedList only accepts data of type :{self.datatype}!")
@@ -53,9 +64,10 @@ class LinkedList :
         
         Current = self.Head
         while Current.next_node is not None :
-            if Current.next_node == None :
-                Current = Current.next_node.next_node
-            Current = Current.next_Node
+            if Current.next_node.Value == Node.Value :
+                Current.next_node = Current.next_node.next_node
+                break
+            Current = Current.next_node
     
     def DeleteAnIndex(self , Index : int):
         Current = self.Head
@@ -91,19 +103,44 @@ def MergeSorted(List1 :LinkedList , List2 : LinkedList):
         current2 = current2.next_node
     
     return merged_list
+def GetMiddle(linked_list: LinkedList) -> LinkedList:
+    if not linked_list.Head or not linked_list.Head.next_node:
+        return linked_list
 
+    slow = linked_list.Head
+    fast = linked_list.Head.next_node
+
+    while fast and fast.next_node:
+        slow = slow.next_node
+        fast = fast.next_node.next_node
+    
+    middle = LinkedList(linked_list.datatype)
+    middle.Head = slow.next_node
+    slow.next_node = None
+
+    return middle
+def Sort(linked_list: LinkedList) -> LinkedList:
+    if not linked_list.Head or not linked_list.Head.next_node:
+        return linked_list
+
+    middle = GetMiddle(linked_list)
+
+    left_sorted = Sort(linked_list)
+    right_sorted = Sort(middle)
+
+    return MergeSorted(left_sorted, right_sorted)
 
 test = LinkedList(int)
 
 test.Insert(5)
 test.Insert(6)
 test.Insert(7)
-test.Insert(8)
+test.Insert(3)
 test.Insert(9)
 test.Insert(10)
 test.Insert(8)
-
-print(test) 
-test.DeleteANode(test.GetNodeWithValue(5))
-
 print(test)
+
+test.DeleteANode(test.GetNodeWithValue(3))
+
+print(Sort(test))
